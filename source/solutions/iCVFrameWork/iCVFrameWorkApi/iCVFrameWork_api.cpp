552b721077bb3579ca038286da0e7ccbdd2c23f8
wensha2 <shawen@mail.ustc.edu.cn>
2024-05-15 11:04:39 +0800

    [add] api initiail code

--- source/solutions/iCVFrameWork/iCVFrameWorkApi/iCVFrameWork_api.cpp
+++ source/solutions/iCVFrameWork/iCVFrameWorkApi/iCVFrameWork_api.cpp
@@ -3,6 +3,16 @@
 #include "iCVFrameWork_config.h"
 #include "iCVFrameWork_mgr.hpp"
 
+static bool g_iCVFrameWork_init = false;
+static bool g_iCVFrameWork_permission = false;
+static std::string g_iCVFrameWork_engine_version = "";
+
+#ifdef SMOKING_VERSION
+#define VERSION_TAG "Test"
+#else
+#define VERSION_TAG ""
+#endif
+
 int32_t ICVBASEAPI iCVFrameWorkInitialize(const char *cfg_path,
                                           void *reserved) {
     int32_t ret = ICVBASE_NO_ERROR;
@@ -10,6 +20,24 @@ int32_t ICVBASEAPI iCVFrameWorkInitialize(const char *cfg_path,
     srlog_open(cfg_path,
                G_CFG_ICVFRAMEWORK()->get_iCVFrameWork_LogPath().c_str());
     srlog_perf(LOG_PROF_TAG, "API");
+    srlog_verify_init(!g_iCVFrameWork_init, ICVBASE_INIT_ERROR);
+    std::string engine_name = ENGINE_NAME;
+    std::string engine_version_majar = ENGINE_VERSION_MAJOR;
+    std::string engine_version_minor = ENGINE_VERSION_MINOR;
+    std::string engine_version_patch = ENGINE_VERSION_PATCH;
+    std::string engine_version =
+        engine_name + VERSION_TAG + engine_version_majar + "." +
+        engine_version_minor + "." + engine_version_patch;
+    std::cout << "Engine Version : " << engine_version << std::endl;
+    srlog_info("Engine Version : {}", engine_version);
+    g_iCVFrameWork_engine_version = engine_version;
+    g_iCVFrameWork_permission = true; // TODO
+    ret = G_iCVFrameWorkInstMgr()->init(cfg_path);
+    srlog_error_return(!ret, ("G_iCVFrameWorkInstMgr()->init( {} )", cfg_path),
+                       ret);
+    g_iCVFrameWork_init = true;
+    std::cout << "Initialize successful ..." << std::endl;
+    srlog_info("Initialize successful ...");
     return ret;
 }
 
