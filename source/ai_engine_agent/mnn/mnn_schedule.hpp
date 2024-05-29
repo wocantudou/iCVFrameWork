a8984f144c3d8ebff927db8536a1ad4b594363e3
wensha2 <shawen@mail.ustc.edu.cn>
2024-05-29 16:17:37 +0800

    [update] schedule

---
+++ source/ai_engine_agent/mnn/mnn_schedule.hpp
@@ -0,0 +1,173 @@
+#pragma once
+#include <memory>
+// MNN
+#include "MNN/Interpreter.hpp"
+#include "MNN/MNNForwardType.h"
+#include "iCVBase_errcode.h"
+#include "iCVBase_types.h"
+#include "iCVLoggerApi.h"
+#define RES_MEM_IDX 0
+#define RES_DESC_IDX 1
+#define RES_IDX_COUNT 2
+
+namespace DNN_WRAPPER {
+
+typedef struct _DNNModel {
+    char *res_buffer_[RES_IDX_COUNT];
+    uint64_t res_buffer_len_[RES_IDX_COUNT];
+    std::shared_ptr<MNN::Interpreter> net_;
+} DNNModel;
+typedef DNNModel *DNNModelHandle;
+
+typedef struct _DNNExecute {
+    MNN::Session *session_;
+} DNNExecute;
+typedef DNNExecute *DNNExecuteHandle;
+
+class AIEngineSchedule {
+  public:
+    AIEngineSchedule() {}
+    ~AIEngineSchedule() {}
+
+  public:
+    int32_t ai_engine_create_impl(DNNModelHandle &model_handle) {
+        int32_t ret = ICVBASE_NO_ERROR;
+        std::string model_json_info =
+            std::string(model_handle->res_buffer_[RES_DESC_IDX],
+                        model_handle->res_buffer_len_[RES_DESC_IDX]);
+        srlog_trace("model info: {}", model_json_info);
+        model_handle->net_ = std::shared_ptr<MNN::Interpreter>(
+            MNN::Interpreter::createFromBuffer(
+                model_handle->res_buffer_[RES_MEM_IDX],
+                model_handle->res_buffer_len_[RES_MEM_IDX]));
+        return ret;
+    }
+    int32_t ai_engine_destory_impl(DNNModelHandle &model_handle) {
+        int32_t ret = ICVBASE_NO_ERROR;
+        if (model_handle) {
+            model_handle->net_->releaseModel();
+            delete model_handle;
+            model_handle = NULL;
+        }
+        return ret;
+    }
+    int32_t ai_inst_create_impl(DNNModelHandle &model_handle,
+                                DNNExecuteHandle &execute_handle) {
+        int32_t ret = ICVBASE_NO_ERROR;
+        MNN::ScheduleConfig config;
+        config.numThread = 1;
+        config.backupType = MNN_FORWARD_CPU;
+        MNN::BackendConfig backendConfig;
+        backendConfig.memory =
+            MNN::BackendConfig::MemoryMode::Memory_Normal; // 内存
+        backendConfig.power =
+            MNN::BackendConfig::PowerMode::Power_Normal; // 功耗
+        backendConfig.precision =
+            MNN::BackendConfig::PrecisionMode::Precision_Normal; // 精度
+        config.backendConfig = &backendConfig;
+        execute_handle->session_ = model_handle->net_->createSession(config);
+        return ret;
+    }
+    int32_t ai_inst_destroy_impl(DNNModelHandle &model_handle,
+                                 DNNExecuteHandle &execute_handle) {
+        int32_t ret = ICVBASE_NO_ERROR;
+        if (execute_handle) {
+            bool flag =
+                model_handle->net_->releaseSession(execute_handle->session_);
+            srlog_error_return(!ret, ("releaseSession() error!"),
+                               ICVBASE_INSTANCE_ERROR);
+            delete execute_handle;
+            execute_handle = NULL;
+        }
+        return ret;
+    }
+    int32_t ai_io_data_prepares_impl(DNNModelHandle &model_handle,
+                                     DNNExecuteHandle &execute_handle,
+                                     DnnDataIO &data_io) {
+        int32_t ret = ICVBASE_NO_ERROR;
+        // input prepares{
+        data_io.inputs_.clear();
+        auto input_tensors =
+            model_handle->net_->getSessionInputAll(execute_handle->session_);
+        for (auto &input_tensor_kv : input_tensors) {
+            DnnDataInfo tmp;
+            tmp.dims_.nbDims = input_tensor_kv.second->dimensions();
+            for (int i = 0; i < input_tensor_kv.second->dimensions(); i++) {
+                tmp.dims_.d[i] = input_tensor_kv.second->shape()[i];
+            }
+            tmp.data_.resize(tmp.dims_.count());
+            data_io.inputs_.insert(std::make_pair(input_tensor_kv.first, tmp));
+        }
+        // input prepares}
+
+        // output prepares{
+        data_io.outputs_.clear();
+        auto output_tensors =
+            model_handle->net_->getSessionOutputAll(execute_handle->session_);
+        for (auto &output_tensor_kv : output_tensors) {
+            DnnDataInfo tmp;
+            tmp.dims_.nbDims = output_tensor_kv.second->dimensions();
+            for (int i = 0; i < output_tensor_kv.second->dimensions(); i++) {
+                tmp.dims_.d[i] = output_tensor_kv.second->shape()[i];
+            }
+            tmp.data_.resize(tmp.dims_.count());
+            data_io.outputs_.insert(
+                std::make_pair(output_tensor_kv.first, tmp));
+        }
+        // output prepares}
+        return ret;
+    }
+    int32_t ai_run_impl(DNNModelHandle &model_handle,
+                        DNNExecuteHandle &execute_handle, DnnDataIO &data_io) {
+        int32_t ret = ICVBASE_NO_ERROR;
+        // set input
+        auto input_tensors =
+            model_handle->net_->getSessionInputAll(execute_handle->session_);
+        for (auto &input_kv : data_io.inputs_) {
+            auto nchw_input = std::make_shared<MNN::Tensor>(
+                input_tensors.at(input_kv.first), MNN::Tensor::CAFFE);
+            auto nchw_input_host = nchw_input->host<float>();
+            ::memcpy(nchw_input_host, input_kv.second.data_.data(),
+                     input_kv.second.data_.size() * sizeof(float));
+            bool flag = input_tensors.at(input_kv.first)
+                            ->copyFromHostTensor(nchw_input.get());
+            srlog_error_return(
+                flag, ("copyFromHostTensor( {} ) error!", input_kv.first),
+                ICVBASE_MEMORY_ERROR);
+        }
+
+        // run
+        ret = model_handle->net_->runSession(execute_handle->session_);
+        srlog_error_return(!ret, ("runSession() error!"),
+                           ICVBASE_ALGORITHM_EXECUTION_ERROR);
+
+        // get output
+        auto output_tensors =
+            model_handle->net_->getSessionOutputAll(execute_handle->session_);
+
+        for (auto &output_tensor_kv : output_tensors) {
+            auto nchw_output = std::make_shared<MNN::Tensor>(
+                output_tensor_kv.second, MNN::Tensor::CAFFE);
+            output_tensor_kv.second->copyToHostTensor(nchw_output.get());
+            // get output data
+            data_io.outputs_.at(output_tensor_kv.first)
+                .data_.resize(nchw_output->elementSize());
+            ::memcpy(data_io.outputs_.at(output_tensor_kv.first).data_.data(),
+                     nchw_output->host<float>(), nchw_output->size());
+            // get output shape
+            data_io.outputs_.at(output_tensor_kv.first).dims_.nbDims =
+                nchw_output->dimensions();
+            for (int i = 0; i < nchw_output->dimensions(); i++) {
+                data_io.outputs_.at(output_tensor_kv.first).dims_.d[i] =
+                    nchw_output->shape()[i];
+            }
+        }
+        return ret;
+    }
+    int32_t ai_run_batch_impl() {
+        int32_t ret = ICVBASE_NO_ERROR;
+        // TODO
+        return ret;
+    }
+};
+} // namespace DNN_WRAPPER
--- source/ai_engine_agent/mnn/mnn_wrapper.h
+++ source/ai_engine_agent/mnn/mnn_wrapper.h
@@ -1,32 +1,11 @@
 #pragma once
 #include "dnn_wrapper/dnn_wrapper.hpp"
-#include "iCVBase_errcode.h"
-#include "iCVBase_types.h"
-#include "iCVLoggerApi.h"
 #include "iCVProfiler.hpp"
-// MNN
-#include "MNN/Interpreter.hpp"
-#include "MNN/MNNForwardType.h"
+#include "mnn_schedule.hpp"
 
 #define LOG_PROF_TAG "LogProf"
 
-#define RES_MEM_IDX 0
-#define RES_DESC_IDX 1
-#define RES_IDX_COUNT 2
-
 namespace DNN_WRAPPER {
-
-typedef struct _DNNModel {
-    char *res_buffer_[RES_IDX_COUNT];
-    uint64_t res_buffer_len_[RES_IDX_COUNT];
-    std::shared_ptr<MNN::Interpreter> net_;
-} DNNModel;
-typedef DNNModel *DNNModelHandle;
-
-typedef struct _DNNExecute {
-    MNN::Session *session_;
-} DNNExecute;
-typedef DNNExecute *DNNExecuteHandle;
 
 class MNNWrapper
     : public DNNWrapper<RESTYPE, DNNModelHandle, DNNExecuteHandle> {
@@ -67,15 +46,11 @@ class MNNWrapper
             model_handle->res_buffer_len_[idx] =
                 dnn_res_map_.at(res_type)->get_mem_len_by_idx(idx);
         }
-        std::string model_json_info =
-            std::string(model_handle->res_buffer_[RES_DESC_IDX],
-                        model_handle->res_buffer_len_[RES_DESC_IDX]);
-        srlog_trace("{} model info: {}", static_cast<int>(res_type),
-                    model_json_info);
-        model_handle->net_ = std::shared_ptr<MNN::Interpreter>(
-            MNN::Interpreter::createFromBuffer(
-                model_handle->res_buffer_[RES_MEM_IDX],
-                model_handle->res_buffer_len_[RES_MEM_IDX]));
+        ret = ai_engine_schedule_.ai_engine_create_impl(model_handle);
+        srlog_error_return(
+            !ret,
+            ("ai_engine_create_impl( {} ) error!", static_cast<int>(res_type)),
+            ret);
         ret = push_model_handle(res_type, model_handle);
         srlog_error_return(
             !ret,
@@ -102,16 +77,11 @@ class MNNWrapper
             model_handle->res_buffer_len_[idx] =
                 dnn_res_map_.at(res_type)->get_mem_len_by_idx(idx);
         }
-        std::string model_json_info =
-            std::string(model_handle->res_buffer_[RES_DESC_IDX],
-                        model_handle->res_buffer_len_[RES_DESC_IDX]);
-        srlog_trace("{} model info: {}", static_cast<int>(res_type),
-                    model_json_info);
-        std::shared_ptr<MNN::Interpreter> net =
-            std::shared_ptr<MNN::Interpreter>(
-                MNN::Interpreter::createFromBuffer(
-                    model_handle->res_buffer_[RES_MEM_IDX],
-                    model_handle->res_buffer_len_[RES_MEM_IDX]));
+        ret = ai_engine_schedule_.ai_engine_create_impl(model_handle);
+        srlog_error_return(
+            !ret,
+            ("ai_engine_create_impl( {} ) error!", static_cast<int>(res_type)),
+            ret);
         ret = push_model_handle(res_type, model_handle);
         srlog_error_return(
             !ret,
@@ -130,12 +100,11 @@ class MNNWrapper
             !ret,
             ("pull_model_handle( {} ) error!", static_cast<int>(res_type)),
             ret);
-
-        if (model_handle) {
-            model_handle->net_->releaseModel();
-            delete model_handle;
-            model_handle = NULL;
-        }
+        ret = ai_engine_schedule_.ai_engine_destory_impl(model_handle);
+        srlog_error_return(
+            !ret,
+            ("ai_engine_destory_impl( {} ) error!", static_cast<int>(res_type)),
+            ret);
         ret = del_res_from_mem(res_type);
         srlog_error_return(
             !ret, ("del_res_from_mem( {} ) error!", static_cast<int>(res_type)),
@@ -143,13 +112,10 @@ class MNNWrapper
         return ret;
     }
 
-    int32_t data_io_prepares(const RESTYPE res_type, DnnDataIO &io_inst) {
+    int32_t data_io_prepares(const RESTYPE res_type, DnnDataIO &data_io) {
         int32_t ret = ICVBASE_NO_ERROR;
         srlog_perf(LOG_PROF_TAG, "MNNWrapper");
         srlog_verify_init(is_init_, ICVBASE_INIT_ERROR);
-        io_inst.inputs_.clear();
-        io_inst.outputs_.clear();
-
         DNNModelHandle model_handle = dnn_res_map_.at(res_type)->model_handle_;
         std::shared_ptr<DnnInst> inst;
         ret = pop_instpool_map_inst_by_rt(res_type, inst);
@@ -157,34 +123,12 @@ class MNNWrapper
                            ("pop_instpool_map_inst_by_rt( {} ) error!",
                             static_cast<int>(res_type)),
                            ret);
-        // input prepares{
-        auto input_tensors = model_handle->net_->getSessionInputAll(
-            inst->execute_handle_->session_);
-        for (auto &input_tensor_kv : input_tensors) {
-            DnnDataInfo tmp;
-            tmp.dims_.nbDims = input_tensor_kv.second->dimensions();
-            for (int i = 0; i < input_tensor_kv.second->dimensions(); i++) {
-                tmp.dims_.d[i] = input_tensor_kv.second->shape()[i];
-            }
-            tmp.data_.resize(tmp.dims_.count());
-            io_inst.inputs_.insert(std::make_pair(input_tensor_kv.first, tmp));
-        }
-        // input prepares}
-
-        // output prepares{
-        auto output_tensors = model_handle->net_->getSessionOutputAll(
-            inst->execute_handle_->session_);
-        for (auto &output_tensor_kv : output_tensors) {
-            DnnDataInfo tmp;
-            tmp.dims_.nbDims = output_tensor_kv.second->dimensions();
-            for (int i = 0; i < output_tensor_kv.second->dimensions(); i++) {
-                tmp.dims_.d[i] = output_tensor_kv.second->shape()[i];
-            }
-            tmp.data_.resize(tmp.dims_.count());
-            io_inst.outputs_.insert(
-                std::make_pair(output_tensor_kv.first, tmp));
-        }
-        // output prepares}
+        ret = ai_engine_schedule_.ai_io_data_prepares_impl(
+            model_handle, inst->execute_handle_, data_io);
+        srlog_error_return(!ret,
+                           ("ai_io_data_prepares_impl( {} ) error!",
+                            static_cast<int>(res_type)),
+                           ret);
         ret = push_instpool_map_inst_by_rt(res_type, inst);
         srlog_error_return(!ret,
                            ("push_instpool_map_inst_by_rt( {} ) error!",
@@ -192,7 +136,7 @@ class MNNWrapper
                            ret);
         return ret;
     }
-    int32_t inference(const RESTYPE res_type, DnnDataIO &io_inst,
+    int32_t inference(const RESTYPE res_type, DnnDataIO &data_io,
                       bool batch_mode = false) {
         int32_t ret = ICVBASE_NO_ERROR;
         srlog_perf(LOG_PROF_TAG, "MNNWrapper");
@@ -204,50 +148,11 @@ class MNNWrapper
                            ("pop_instpool_map_inst_by_rt( {} ) error!",
                             static_cast<int>(res_type)),
                            ret);
-        // set input
-        auto input_tensors = model_handle->net_->getSessionInputAll(
-            inst->execute_handle_->session_);
-        for (auto &input_kv : io_inst.inputs_) {
-            auto nchw_input = std::make_shared<MNN::Tensor>(
-                input_tensors.at(input_kv.first), MNN::Tensor::CAFFE);
-            auto nchw_input_host = nchw_input->host<float>();
-            ::memcpy(nchw_input_host, input_kv.second.data_.data(),
-                     input_kv.second.data_.size() * sizeof(float));
-            bool flag = input_tensors.at(input_kv.first)
-                            ->copyFromHostTensor(nchw_input.get());
-            srlog_error_return(
-                flag, ("copyFromHostTensor( {} ) error!", input_kv.first),
-                ICVBASE_MEMORY_ERROR);
-        }
-
-        // run
-        ret = model_handle->net_->runSession(inst->execute_handle_->session_);
+        ret = ai_engine_schedule_.ai_run_impl(model_handle,
+                                              inst->execute_handle_, data_io);
         srlog_error_return(
-            !ret, ("runSession( {} ) error!", static_cast<int>(res_type)),
-            ICVBASE_ALGORITHM_EXECUTION_ERROR);
-
-        // get output
-        auto output_tensors = model_handle->net_->getSessionOutputAll(
-            inst->execute_handle_->session_);
-
-        for (auto &output_tensor_kv : output_tensors) {
-            auto nchw_output = std::make_shared<MNN::Tensor>(
-                output_tensor_kv.second, MNN::Tensor::CAFFE);
-            output_tensor_kv.second->copyToHostTensor(nchw_output.get());
-            // get output data
-            io_inst.outputs_.at(output_tensor_kv.first)
-                .data_.resize(nchw_output->elementSize());
-            ::memcpy(io_inst.outputs_.at(output_tensor_kv.first).data_.data(),
-                     nchw_output->host<float>(), nchw_output->size());
-            // get output shape
-            io_inst.outputs_.at(output_tensor_kv.first).dims_.nbDims =
-                nchw_output->dimensions();
-            for (int i = 0; i < nchw_output->dimensions(); i++) {
-                io_inst.outputs_.at(output_tensor_kv.first).dims_.d[i] =
-                    nchw_output->shape()[i];
-            }
-        }
-
+            !ret, ("ai_run_impl( {} ) error!", static_cast<int>(res_type)),
+            ret);
         ret = push_instpool_map_inst_by_rt(res_type, inst);
         srlog_error_return(!ret,
                            ("push_instpool_map_inst_by_rt( {} ) error!",
@@ -256,7 +161,7 @@ class MNNWrapper
         return ret;
     }
 
-    int32_t create_inst(const RESTYPE res_type, DnnScheduleConfig scfg,
+    int32_t create_inst(const RESTYPE res_type, const DnnScheduleConfig &scfg,
                         bool batch_mode = false) {
         int32_t ret = ICVBASE_NO_ERROR;
         srlog_perf(LOG_PROF_TAG, "MNNWrapper");
@@ -264,18 +169,12 @@ class MNNWrapper
         DNNModelHandle model_handle = dnn_res_map_.at(res_type)->model_handle_;
         DNNExecuteHandle execute_handle = new (std::nothrow) DNNExecute();
         srlog_verify_ptr(execute_handle, ICVBASE_MEMORY_ERROR);
-        MNN::ScheduleConfig config;
-        config.numThread = 1;
-        config.backupType = MNN_FORWARD_CPU;
-        MNN::BackendConfig backendConfig;
-        backendConfig.memory =
-            MNN::BackendConfig::MemoryMode::Memory_Normal; // 内存
-        backendConfig.power =
-            MNN::BackendConfig::PowerMode::Power_Normal; // 功耗
-        backendConfig.precision =
-            MNN::BackendConfig::PrecisionMode::Precision_Normal; // 精度
-        config.backendConfig = &backendConfig;
-        execute_handle->session_ = model_handle->net_->createSession(config);
+        ret = ai_engine_schedule_.ai_inst_create_impl(model_handle,
+                                                      execute_handle);
+        srlog_error_return(
+            !ret,
+            ("ai_inst_create_impl( {} ) error!", static_cast<int>(res_type)),
+            ret);
         ret = create_instpool_map_inst_by_rt(res_type, execute_handle,
                                              batch_mode);
         srlog_error_return(!ret,
@@ -296,17 +195,17 @@ class MNNWrapper
                            ("destroy_instpool_map_inst_by_rt( {} ) error!",
                             static_cast<int>(res_type)),
                            ret);
-
-        if (execute_handle) {
-            bool flag =
-                model_handle->net_->releaseSession(execute_handle->session_);
-            srlog_error_return(!ret, ("releaseSession() error!"),
-                               ICVBASE_INSTANCE_ERROR);
-            delete execute_handle;
-            execute_handle = NULL;
-        }
+        ret = ai_engine_schedule_.ai_inst_destroy_impl(model_handle,
+                                                       execute_handle);
+        srlog_error_return(
+            !ret,
+            ("ai_inst_destroy_impl( {} ) error!", static_cast<int>(res_type)),
+            ret);
         return ret;
     }
+
+  private:
+    AIEngineSchedule ai_engine_schedule_;
 
   public:
     static const std::map<RESTYPE, const std::string> restype_name_map_;
