08d9441b62508f650d405fc146ced44c0776a6b4
wensha2 <shawen@mail.ustc.edu.cn>
2024-07-04 18:53:49 +0800

    [update] python api

--- source/solutions/iCVFrameWork/iCVFrameWorkPythonApi/iCVFrameWork_pybind.cpp
+++ source/solutions/iCVFrameWork/iCVFrameWorkPythonApi/iCVFrameWork_pybind.cpp
@@ -7,19 +7,149 @@
 namespace py = pybind11;
 
 PYBIND11_MODULE(iCVFrameWorkPY, m) {
-    m.def(
-        "iCVFrameWorkInitialize",
-        [](const std::string &cfg_path) {
-            return iCVFrameWorkInitialize(cfg_path.c_str(), nullptr);
-        },
-        py::arg("cfg_path"));
+    // Define RESTYPE enum in Python
+    py::enum_<RESTYPE>(m, "RESTYPE")
+        .value("RESTYPE_NONE", RESTYPE_NONE)
+        .value("RESTYPE_FACE_DETECT_CPU", RESTYPE_FACE_DETECT_CPU)
+        .value("RESTYPE_FACE_DETECT_NPU", RESTYPE_FACE_DETECT_NPU)
+        .value("RESTYPE_FACE_ALIGNMENT_CPU", RESTYPE_FACE_ALIGNMENT_CPU)
+        .value("RESTYPE_FACE_ALIGNMENT_NPU", RESTYPE_FACE_ALIGNMENT_NPU)
+        .value("MODTYPE_FACE_SINGLETRACK_CPU", MODTYPE_FACE_SINGLETRACK_CPU)
+        .value("MODTYPE_FACE_SINGLETRACK_NPU", MODTYPE_FACE_SINGLETRACK_NPU)
+        .value("RESTYPE_COUNT", RESTYPE_COUNT)
+        .export_values();
 
-    m.def("iCVFrameWorkUninitialize", &iCVFrameWorkUninitialize);
+    // Bind ICV_BASE_IMG_FMT enum
+    py::enum_<ICV_BASE_IMG_FMT>(m, "ICV_BASE_IMG_FMT")
+        .value("ICV_BASE_IMG_FMT_NONE", ICV_BASE_IMG_FMT_NONE)
+        .value("ICV_BASE_IMG_FMT_GRAY", ICV_BASE_IMG_FMT_GRAY)
+        .value("ICV_BASE_IMG_FMT_RGB", ICV_BASE_IMG_FMT_RGB)
+        .value("ICV_BASE_IMG_FMT_BGR", ICV_BASE_IMG_FMT_BGR)
+        .value("ICV_BASE_IMG_FMT_BGRA", ICV_BASE_IMG_FMT_BGRA)
+        .value("ICV_BASE_IMG_FMT_YUV420_NV12", ICV_BASE_IMG_FMT_YUV420_NV12)
+        .value("ICV_BASE_IMG_FMT_YUV420_NV21", ICV_BASE_IMG_FMT_YUV420_NV21)
+        .value("ICV_BASE_IMG_FMT_COUNT", ICV_BASE_IMG_FMT_COUNT)
+        .export_values();
 
+    // Bind ICV_BASE_DATA_STAT enum
+    py::enum_<ICV_BASE_DATA_STAT>(m, "ICV_BASE_DATA_STAT")
+        .value("ICV_BASE_DATA_STAT_NONE", ICV_BASE_DATA_STAT_NONE)
+        .value("ICV_BASE_DATA_STAT_IMG", ICV_BASE_DATA_STAT_IMG)
+        .value("ICV_BASE_DATA_STAT_AUD", ICV_BASE_DATA_STAT_AUD)
+        .value("ICV_BASE_DATA_STAT_TXT", ICV_BASE_DATA_STAT_TXT)
+        .value("ICV_BASE_DATA_STAT_IMG_AUD", ICV_BASE_DATA_STAT_IMG_AUD)
+        .value("ICV_BASE_DATA_STAT_IMG_TXT", ICV_BASE_DATA_STAT_IMG_TXT)
+        .value("ICV_BASE_DATA_STAT_IMG_AUD_TXT", ICV_BASE_DATA_STAT_IMG_AUD_TXT)
+        .value("ICV_BASE_DATA_STAT_COUNT", ICV_BASE_DATA_STAT_COUNT)
+        .export_values();
+
+    // Bind structures
+    py::class_<ICVBaseRect2f>(m, "ICVBaseRect2f")
+        .def(py::init<>())
+        .def_readwrite("x", &ICVBaseRect2f::x)
+        .def_readwrite("y", &ICVBaseRect2f::y)
+        .def_readwrite("w", &ICVBaseRect2f::w)
+        .def_readwrite("h", &ICVBaseRect2f::h);
+
+    py::class_<ICVBaseRect2fWithInfo>(m, "ICVBaseRect2fWithInfo")
+        .def(py::init<>())
+        .def_readwrite("rect", &ICVBaseRect2fWithInfo::rect)
+        .def_readwrite("conf_score", &ICVBaseRect2fWithInfo::conf_score)
+        .def_readwrite("id", &ICVBaseRect2fWithInfo::id);
+
+    py::class_<ICVBasePoint2f>(m, "ICVBasePoint2f")
+        .def(py::init<>())
+        .def_readwrite("x", &ICVBasePoint2f::x)
+        .def_readwrite("y", &ICVBasePoint2f::y);
+
+    py::class_<ICVBaseNumericFloat>(m, "ICVBaseNumericFloat")
+        .def(py::init<>())
+        .def_readwrite("valid", &ICVBaseNumericFloat::valid)
+        .def_readwrite("val", &ICVBaseNumericFloat::val);
+
+    py::class_<ICVBasePoint2fWithInfo>(m, "ICVBasePoint2fWithInfo")
+        .def(py::init<>())
+        .def_readwrite("point", &ICVBasePoint2fWithInfo::point)
+        .def_readwrite("visiable_info", &ICVBasePoint2fWithInfo::visiable_info)
+        .def_readwrite("conf_score", &ICVBasePoint2fWithInfo::conf_score);
+
+    py::class_<ICVBasePoint3f>(m, "ICVBasePoint3f")
+        .def(py::init<>())
+        .def_readwrite("x", &ICVBasePoint3f::x)
+        .def_readwrite("y", &ICVBasePoint3f::y)
+        .def_readwrite("z", &ICVBasePoint3f::z);
+
+    py::class_<ICVBaseAngle2d>(m, "ICVBaseAngle2d")
+        .def(py::init<>())
+        .def_readwrite("pitch", &ICVBaseAngle2d::pitch)
+        .def_readwrite("yaw", &ICVBaseAngle2d::yaw);
+
+    py::class_<ICVBaseAngle3d>(m, "ICVBaseAngle3d")
+        .def(py::init<>())
+        .def_readwrite("pitch", &ICVBaseAngle3d::pitch)
+        .def_readwrite("yaw", &ICVBaseAngle3d::yaw)
+        .def_readwrite("roll", &ICVBaseAngle3d::roll);
+
+    py::class_<ICVBaseImgDataIn>(m, "ICVBaseImgDataIn")
+        .def(py::init<>())
+        .def_readwrite("data", &ICVBaseImgDataIn::data)
+        .def_readwrite("w", &ICVBaseImgDataIn::w)
+        .def_readwrite("h", &ICVBaseImgDataIn::h)
+        .def_readwrite("data_byte_size", &ICVBaseImgDataIn::data_byte_size)
+        .def_readwrite("fmt", &ICVBaseImgDataIn::fmt)
+        .def_readwrite("next", &ICVBaseImgDataIn::next);
+
+    py::class_<ICVBaseAudDataIn>(m, "ICVBaseAudDataIn")
+        .def(py::init<>())
+        .def_readwrite("data", &ICVBaseAudDataIn::data)
+        .def_readwrite("data_byte_size", &ICVBaseAudDataIn::data_byte_size)
+        .def_readwrite("sample_rate", &ICVBaseAudDataIn::sample_rate)
+        .def_readwrite("sample_bits", &ICVBaseAudDataIn::sample_bits)
+        .def_readwrite("channel_num", &ICVBaseAudDataIn::channel_num)
+        .def_readwrite("next", &ICVBaseAudDataIn::next);
+
+    py::class_<ICVBaseTxtDataIn>(m, "ICVBaseTxtDataIn")
+        .def(py::init<>())
+        .def_readwrite("data", &ICVBaseTxtDataIn::data)
+        .def_readwrite("data_byte_size", &ICVBaseTxtDataIn::data_byte_size)
+        .def_readwrite("next", &ICVBaseTxtDataIn::next);
+
+    py::class_<ICVBaseDataIn>(m, "ICVBaseDataIn")
+        .def(py::init<>())
+        .def_readwrite("data_stat", &ICVBaseDataIn::data_stat)
+        .def_readwrite("img", &ICVBaseDataIn::img)
+        .def_readwrite("aud", &ICVBaseDataIn::aud)
+        .def_readwrite("txt", &ICVBaseDataIn::txt);
+
+    py::class_<ICVFrameWorkFace>(m, "ICVFrameWorkFace")
+        .def(py::init<>())
+        .def_readwrite("face_rect_with_info",
+                       &ICVFrameWorkFace::face_rect_with_info);
+
+    py::class_<ICVFrameWorkFaces>(m, "ICVFrameWorkFaces")
+        .def(py::init<>())
+        .def_property(
+            "faces",
+            [](const ICVFrameWorkFaces &self) {
+                return std::vector<ICVFrameWorkFace>(
+                    self.faces, self.faces + self.face_num);
+            },
+            [](ICVFrameWorkFaces &self,
+               const std::vector<ICVFrameWorkFace> &faces) {
+                size_t n = std::min(faces.size(),
+                                    static_cast<size_t>(MAX_DETECT_OBJECT_NUM));
+                for (size_t i = 0; i < n; ++i) {
+                    self.faces[i] = faces[i];
+                }
+                self.face_num = n;
+            })
+        .def_readwrite("face_num", &ICVFrameWorkFaces::face_num);
+
+    // Bind functions
     m.def(
         "iCVFrameWorkCreateInst",
         [](RESTYPE res_type) {
-            iCVFrameWork_INST inst;
+            iCVFrameWork_INST inst = nullptr;
             int32_t ret = iCVFrameWorkCreateInst(&inst, res_type);
             return std::make_pair(ret, inst);
         },
@@ -43,7 +173,7 @@ PYBIND11_MODULE(iCVFrameWorkPY, m) {
             std::string value(len, '\0');
             int32_t ret =
                 iCVFrameWorkGetParameter(inst, param.c_str(), &value[0], len);
-            return std::make_pair(ret, value);
+            return std::make_pair(ret, std::string(value.c_str()));
         },
         py::arg("inst"), py::arg("param"), py::arg("len"));
 
@@ -87,7 +217,8 @@ PYBIND11_MODULE(iCVFrameWorkPY, m) {
     m.def("iCVFrameWorkGetExpiredTime", []() {
         const char *expired_time;
         int32_t ret = iCVFrameWorkGetExpiredTime(&expired_time);
-        return std::make_pair(ret, std::string(expired_time));
+        return std::make_pair(ret,
+                              std::string(expired_time ? expired_time : ""));
     });
 
     m.def(
@@ -102,6 +233,7 @@ PYBIND11_MODULE(iCVFrameWorkPY, m) {
     m.def("iCVFrameWorkGetEngineVersion", []() {
         const char *engine_version;
         int32_t ret = iCVFrameWorkGetEngineVersion(&engine_version);
-        return std::make_pair(ret, std::string(engine_version));
+        return std::make_pair(
+            ret, std::string(engine_version ? engine_version : ""));
     });
 }
